16049c515895b9d943d462191719f5189d0efb68
kksumm123 <kksumm123@gmail.com>
2021-07-01 17:15:46 +0900

    1. Canvas/ScrollView추가 2. Canvas-> Canvas Scaler, ScaleWith Screen 3. ScrollView Stretch 후 앵커 잡아서 1/4크기로 4. Content에 컴포넌트추가 4-1. Grid Layout Group 4-2. Content Size Fitter 5. ScrollView - Scroll Rect Horizontal 체크해제 , Horizontal 하위 개체 삭제

--- Assets/ShopItemData.cs
+++ Assets/ShopItemData.cs
@@ -2,11 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ItemType
+{
+    Weapon,
+    Armor,
+    potion,
+    Etc,
+}
 [System.Serializable]
 public class ShopItemInfo
 {
     [SerializeField] string name;
     [SerializeField] Sprite icon;
+    [SerializeField] ItemType type; 
     [SerializeField] int sellPrice;
     [SerializeField] int butPrice;
     [SerializeField] string description;
