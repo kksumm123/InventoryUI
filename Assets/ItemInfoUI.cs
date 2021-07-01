bf2f286973fe6411decc3c2dbf72717c4f0aa8df
kksumm123 <kksumm123@gmail.com>
2021-07-01 17:46:06 +0900

    ItemInfoUI 추가

---
+++ Assets/ItemInfoUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfoUI : MonoBehaviour
+{
+    Text name;
+    Image itemInfoUI;
+    Image icon;
+    Text description;
+    Button button;
+    private void Start()
+    {
+        name = transform.Find("ItemInfoUI/Name").GetComponent<Text>();
+        itemInfoUI = transform.Find("ItemInfoUI").GetComponent<Image>();
+        icon = transform.Find("ItemInfoUI/Icon").GetComponent<Image>();
+        description = transform.Find("ItemInfoUI/Description").GetComponent<Text>();
+        button = transform.Find("ItemInfoUI/Button").GetComponent<Button>();
+    }
+}
