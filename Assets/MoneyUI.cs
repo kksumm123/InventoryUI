a64f3f3b548feff8f7a52a4d531877fe7a4415a6
kksumm123 <kksumm123@gmail.com>
2021-07-01 20:08:51 +0900

    아 맞다 커밋 Gold, Dia 이미지 추가 UserData.cs 추가 PlayerPrefs 이용하여 저장/불러오기 MoneyUI에서 userdata 싱글턴으로 값 참조

---
+++ Assets/MoneyUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyUI : MonoBehaviour
+{
+    Text goldText;
+    Text diaText;
+    void Start()
+    {
+        goldText = transform.Find("Gold/Text").GetComponent<Text>();
+        diaText = transform.Find("Dia/Text").GetComponent<Text>();
+        goldText.text = UserData.instance.gold.ToString();
+        diaText.text = UserData.instance.dia.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UserData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    public static UserData instance;
+
+    public int gold;
+    public int dia;
+    private void Awake()
+    {
+        instance = this;
+
+        Load();
+    }
+    private void OnDestroy()
+    {
+        Save();
+    }
+    private void Load()
+    {
+        gold = PlayerPrefs.GetInt("gold");
+        dia = PlayerPrefs.GetInt("dia");
+    }
+    private void Save()
+    {
+        PlayerPrefs.SetInt("gold", gold);
+        PlayerPrefs.SetInt("dia", dia);
+    }
+}
