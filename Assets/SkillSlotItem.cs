aa6ad3a30537235f997690c3ea47cb62682d5a10
kksumm123 <kksumm123@gmail.com>
2021-07-02 20:27:49 +0900

    SkillSlotItem.cs 추가 ->IDropHandler 추가

---
+++ Assets/SkillSlotItem.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkillSlotItem : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData data)
+    {
+        if (data.pointerDrag != null)
+        {
+            Debug.Log("Dropped object was: " + data.pointerDrag);
+        }
+    }
+}
