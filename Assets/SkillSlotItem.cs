69afb42bbaf5f0b8ef8b631f1190517c7d1ff813
kksumm123 <kksumm123@gmail.com>
2021-07-02 20:36:38 +0900

    Drop했을 때  Slot의 image 컴포넌트에 Drag된 image를 복사하도록

--- Assets/SkillSlotItem.cs
+++ Assets/SkillSlotItem.cs
@@ -2,14 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SkillSlotItem : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData data)
     {
-        if (data.pointerDrag != null)
-        {
-            Debug.Log("Dropped object was: " + data.pointerDrag);
-        }
+        transform.Find("Image").GetComponent<Image>().sprite = data.pointerClick.GetComponent<Image>().sprite;
     }
 }
